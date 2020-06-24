d82411ad8be3f04ac6d4e7ef3e191b773377de44
ba-p <phuongtr4916@gmail.com>
2020-06-24 13:09:55 +0700

    Updates

---
+++ precache-manifest.cf22ff20bbac7a8744da8169e1196915.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d122eda2a5c64bd4b04fbf3bd1c2d42",
+    "url": "/Blog/index.html"
+  },
+  {
+    "revision": "b9c2035b9f4b903a698f",
+    "url": "/Blog/static/css/main.20fc115e.chunk.css"
+  },
+  {
+    "revision": "0bd3079c15dd1d212382",
+    "url": "/Blog/static/js/2.3e05f28e.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/Blog/static/js/2.3e05f28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c2035b9f4b903a698f",
+    "url": "/Blog/static/js/main.e3d610df.chunk.js"
+  },
+  {
+    "revision": "6bb639fec64f1efdcef4",
+    "url": "/Blog/static/js/runtime-main.aa514de0.js"
+  },
+  {
+    "revision": "377cb82efaef2e3d98b72716c19d54e7",
+    "url": "/Blog/static/media/AVT.377cb82e.JPG"
+  },
+  {
+    "revision": "14aa1d70b3601017fd12fe522627468e",
+    "url": "/Blog/static/media/airbnb-co-muc-thua-lo-hang-tram-trieu-do.14aa1d70.png"
+  },
+  {
+    "revision": "2837bb3f71a83f004e22cbc3c67c7f8d",
+    "url": "/Blog/static/media/firebase-db.2837bb3f.jpg"
+  }
+]);
